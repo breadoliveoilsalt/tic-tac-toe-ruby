9786c05bd876e1542b35f16fb66097fe752eff55
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-17 15:19:10 -0400

    Player test: fail existence

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/player.rb'
+
+RSpec.describe Player do
+
+  it "exists" do 
+    expect(described_class).to equal(Player)
+  end
+
+	xdescribe "#get_score" do
+  end
+
+end
