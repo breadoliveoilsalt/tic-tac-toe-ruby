870ea25171b115c1d6fe42b74b4d2e986586f9be
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-19 12:27:47 -0400

    Board tests: draft mock test to test formatting for testing stdout

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,12 +2,26 @@ require_relative '../lib/board.rb'
 
 describe Board do
 
+  before(:each) do 
+    @board = Board.new
+  end
+
   describe "#new" do
     it "initializes a Board instance with an instance variable @data equal to an array of 9 empty strings" do 
-      @board = Board.new
       expected_output = ["","","","","","","","",""]
       expect(@board.data).to eq(expected_output)
     end
   end
 
+  describe "#display_for_user" do
+    xit "prints a user friendly version of the current board to the terminal" do
+      
+    end
+  end
+
+  describe "mock puts test" do 
+    it "hopefully passes" do
+      expect(@board.test_message).to output("Success!").to_stdout
+    end
+  end
 end
