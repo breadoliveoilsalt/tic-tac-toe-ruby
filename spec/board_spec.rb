cad993253ad95fdf8f2341787d047d0f287b4d2f
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-19 12:12:03 -0400

    Board test: fail #new test initializing data array

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,5 +2,12 @@ require_relative '../lib/board.rb'
 
 describe Board do
 
+  describe "#new" do
+    it "initializes a Board instance with an instance variable @data equal to an array of 9 empty strings" do 
+      @board = Board.new
+      expected_output = ["","","","","","","","",""]
+      expect(@board.data).to eq(expected_output)
+    end
+  end
 
 end
