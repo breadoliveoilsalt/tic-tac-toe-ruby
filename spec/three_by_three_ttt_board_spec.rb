2dc67e17c3680f5b8fc383b7a85134b0a867a0f8
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-20 10:32:03 -0400

    Board Tests: refactor tests to remove constant refs to @board instance variable

--- spec/three_by_three_ttt_board_spec.rb
+++ spec/three_by_three_ttt_board_spec.rb
@@ -6,10 +6,14 @@ describe ThreeByThreeTTTBoard do
     @board = ThreeByThreeTTTBoard.new
   end
 
+  def board
+    @board
+  end
+  
   describe "#new" do
     it "initializes a Board instance with an instance variable @data equal to an array of 9 empty strings" do 
       expected_data = [" "," ", " ", " ", " ", " ", " ", " ", " "]
-      expect(@board.data).to eq(expected_data)
+      expect(board.data).to eq(expected_data)
     end
   end
 
@@ -23,12 +27,12 @@ describe ThreeByThreeTTTBoard do
            |   |  
       MESSAGE
 
-      expect{ @board.display_board_for_user}.to output(expected_message_to_terminal).to_stdout
+      expect{ board.display_board_for_user}.to output(expected_message_to_terminal).to_stdout
     end
 
     it "prints to the terminal a user friendly version of the board that reflects past moves" do
-      @board.data[0] = "X"
-      @board.data[8] = "O"
+      board.data[0] = "X"
+      board.data[8] = "O"
       expected_message_to_terminal = <<~MESSAGE
          X |   |   
         -----------
@@ -36,41 +40,46 @@ describe ThreeByThreeTTTBoard do
         -----------
            |   | O
       MESSAGE
-      expect{ @board.display_board_for_user}.to output(expected_message_to_terminal).to_stdout
+      expect{ board.display_board_for_user}.to output(expected_message_to_terminal).to_stdout
     end
   end
 
   describe "#place_marker_on_board_box" do
     it "places a marker in the data accoring to the box selected" do 
-      @board.place_marker_on_board_box("X", 9)
+      board.place_marker_on_board_box("X", 9)
       expected_data = [" "," ", " ", " ", " ", " ", " ", " ", "X"]
-      expect(@board.data).to eq(expected_data)
+      expect(board.data).to eq(expected_data)
     end
   end
 
   describe "#game_won?" do
     it "returns false when all boxes are empty" do
-      expect(@board.game_won?).to eq(false)
+      expect(board.game_won?).to eq(false)
     end
 
     it "returns true when the first row consists of the same markers and not blank spaces" do 
-      @board.data[0] = "X"
-      @board.data[1] = "X"
-      @board.data[2] = "X"
-      expect(@board.game_won?).to eq(true)
+      board.data[0] = "X"
+      board.data[1] = "X"
+      board.data[2] = "X"
+      expect(board.game_won?).to eq(true)
     end
 
     it "returns true when the same markers on left-to-right diagonal" do 
-      @board.data[0] = "O"
-      @board.data[4] = "O"
-      @board.data[8] = "O"
-      expect(@board.game_won?).to eq(true)
+      board.data[0] = "O"
+      board.data[4] = "O"
+      board.data[8] = "O"
+      expect(board.game_won?).to eq(true)
     end
   end
 
   describe "game_tied?" do
     it "returns false when all boxes are empty" do
-      expect(@board.game_tied?). to eq(false)
+      expect(board.game_tied?). to eq(false)
+    end
+
+    it "returns false when there are empty boxes on the board" do 
+      board.data[0] = "X"
+      puts board
     end
   end
 end
