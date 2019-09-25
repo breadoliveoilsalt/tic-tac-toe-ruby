b5696c3061723f4aaf4addf8c8febc7d141cdb03
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-25 12:47:12 -0400

    Rules Tests: pass tests for #valid_marker?

--- lib/three_by_three_ttt_rules.rb
+++ lib/three_by_three_ttt_rules.rb
@@ -4,4 +4,11 @@ class ThreeByThreeTTTRules
     board.data[box_number - 1] == " "
   end
   
+  def valid_marker?(player_marker_selection)
+    ["X", "x", "O", "o"].each do |acceptable_marker|
+      return true if player_marker_selection == acceptable_marker
+    end
+    false
+  end
+   
 end
--- spec/three_by_three_ttt_rules_spec.rb
+++ spec/three_by_three_ttt_rules_spec.rb
@@ -31,4 +31,51 @@ describe "ThreeByThreeTTTRules" do
   
   end
   
+  describe "#valid_marker?" do
+    it "returns true if passed a capital X" do
+      expect(rules.valid_marker?("X")).to eq(true)
+    end
+    
+    it "returns true if passed a lower case x" do
+      expect(rules.valid_marker?("x")).to eq(true)
+    end
+    
+    it "returns true if passed a capital O" do
+      expect(rules.valid_marker?("O")).to eq(true)
+    end
+    
+    it "returns true if passed a lower case o" do
+      expect(rules.valid_marker?("o")).to eq(true)
+    end
+    
+    it "returns false if passed any other character" do 
+      expect(rules.valid_marker?("4")).to eq(false)
+      expect(rules.valid_marker?("Y")).to eq(false)
+      expect(rules.valid_marker?("p")).to eq(false)
+    end
+  end
+
+#  describe "#game_won?" do
+#    it "returns false when all boxes are empty" do
+#      expect(rules.game_won?(board)).to eq(false)
+#    end
+#
+#    it "returns true when the first row consists of the same markers and not blank spaces" do 
+#      board = ThreeByThreeTTTBoard.new(
+#        ["X", "X", "X", 
+#         " ", " ", " ", 
+#         " ", " ", " "]
+#      )
+#      expect(board.game_won?).to eq(true)
+#    end
+#
+#    it "returns true when the same markers are on left-to-right diagonal" do 
+#      board = ThreeByThreeTTTBoard.new(
+#        ["O", " ", " ", 
+#         " ", "O", " ", 
+#         " ", " ", "O"]
+#      )
+#      expect(board.game_won?).to eq(true)
+#    end
+#  end
 end
