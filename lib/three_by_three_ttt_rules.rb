540242427cae3aaf01331ac3be206a1c9b8370f7
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-25 15:00:18 -0400

    Rules test: pass last test for #valid_move? Refactor existing methods into sub-methods

--- lib/three_by_three_ttt_rules.rb
+++ lib/three_by_three_ttt_rules.rb
@@ -1,8 +1,4 @@
 class ThreeByThreeTTTRules
-
-  def box_is_empty?(board, box_number)
-    board.data[box_number - 1] == " "
-  end
   
   def valid_marker?(player_marker_selection)
     ["X", "x", "O", "o"].each do |acceptable_marker|
@@ -12,11 +8,43 @@ class ThreeByThreeTTTRules
   end
    
   def valid_move?(board, user_selection)
-    data_location = user_selection.to_i - 1
-    if board.data[data_location] == " "
-      return true
+    if valid_move_input?(user_selection)
+      return true if box_is_empty?(board, user_selection)
     end
     false
   end
 
+  #private
+
+  def valid_move_input?(user_selection)
+    an_integer?(user_selection) && within_range?(user_selection) 
+  end
+
+  def get_data_location_corresponding_to(stringified_number)
+    stringified_number.to_i - 1
+  end
+
+  def an_integer?(str)
+    begin
+      true if Integer(str)
+    rescue
+      false
+    end
+  end
+
+  def within_range?(str)
+    int = str.to_i
+    int >= 1 && int <= 9
+  end
+
+  def box_is_empty?(board, box_number)
+    data_location = box_number.to_i - 1
+    board.data[data_location] == " "
+  end
+
 end
+#rules = ThreeByThreeTTTRules.new
+
+#puts rules.check_user_selection_is_an_integer("1")
+#puts rules.check_user_selection_is_an_integer("1.25")
+#puts rules.check_user_selection_is_an_integer("apples")
