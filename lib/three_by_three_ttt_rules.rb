c2ddfbf39f985323d1bca48e70d31556791b77b2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-25 12:36:36 -0400

    Rules Tests: pass tests for #box_is_empty?

--- lib/three_by_three_ttt_rules.rb
+++ lib/three_by_three_ttt_rules.rb
@@ -1,3 +1,7 @@
 class ThreeByThreeTTTRules
 
+  def box_is_empty?(board, box_number)
+    board.data[box_number - 1] == " "
+  end
+  
 end
--- spec/three_by_three_ttt_rules_spec.rb
+++ spec/three_by_three_ttt_rules_spec.rb
@@ -13,4 +13,22 @@ describe "ThreeByThreeTTTRules" do
 
   let(:rules) { ThreeByThreeTTTRules.new }
   let(:board) { ThreeByThreeTTTBoard.new }
+
+  describe "#box_is_empty?" do 
+
+    it "returns true when the box number passed in is equal to a string with a space" do
+      expect(rules.box_is_empty?(board, 3)). to eq(true)
+    end
+
+    it "returns false when the box number passed in is not equal to a string with a space" do
+      board = ThreeByThreeTTTBoard.new(
+        [" ", " ", "X", 
+         " ", " ", " ", 
+         " ", " ", " "]
+      )
+      expect(rules.box_is_empty?(board, 3)). to eq(false)
+    end
+  
+  end
+  
 end
