3ab479113f53ed088c8d685f516a33585f3cb544
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-26 11:27:51 -0400

    Switch to creating and testing a 3x3Player Factory. Fail initial existence test

---
+++ spec/player_factory_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/three_by_three_ttt_player_factory.rb'
+require_relative '../lib/human_player.rb'
+require_relative '../lib/three_by_three_ttt_computer_player.rb'
+
+describe ThreeByThreeTTTPlayerFactory do
+
+  let(:player_factory) { ThreeByThreeTTTPlayerFactory.new }
+  
+end
---
+++ spec/three_by_three_ttt_player_factory_spec.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/three_by_three_ttt_player_factory.rb'
+require_relative '../lib/human_player.rb'
+require_relative '../lib/three_by_three_ttt_computer_player.rb'
