2e7375890da601c2d2bdbb269379cd3d2b5fd9ca
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-27 09:54:42 -0400

    Refactor Player Factory to divide up configuration of human and computer players

--- lib/three_by_three_ttt_player_factory.rb
+++ lib/three_by_three_ttt_player_factory.rb
@@ -3,22 +3,32 @@ require_relative '../lib/three_by_three_ttt_computer_player.rb'
 
 class ThreeByThreeTTTPlayerFactory
 
-  attr_accessor :number_of_players, :number_of_human_players
+  attr_accessor :number_of_players, :number_of_human_players, :players
 
   def initialize(number_of_players: 2, number_of_human_players: 1)
     @number_of_players = number_of_players
     @number_of_human_players = number_of_human_players
+    @players = []
   end
 
   def configure_players
+    configure_human_players
+    configure_computer
+    players
+  end
+
+  def configure_human_player
     human_player = HumanPlayer.new
     human_player.marker = "X"
     human_player.name = "User"
+    players.push(human_player)
+  end
 
+  def configure_computer_players
     computer_player = ThreeByThreeTTTComputerPlayer.new
     computer_player.marker = "O"
     computer_player.name = "Computer Player"
-
-    [human_player, computer_player]
+    players.push(computer_player)
   end
+
 end
