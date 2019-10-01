0a98d7a6db0528a15c09fe91c7f194ec2bcc7e59
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-01 12:44:51 -0400

    UserInput test: pass test for #read_line

--- lib/user_input.rb
+++ lib/user_input.rb
@@ -1,3 +1,13 @@
 class UserInput
 
+  attr_accessor :input_stream
+
+  def initialize(input_stream: $stdin)
+    @input_stream = input_stream
+  end
+
+  def read_line
+    input_stream.gets.chomp
+  end
+  
 end
--- spec/user_input_spec.rb
+++ spec/user_input_spec.rb
@@ -2,4 +2,20 @@ require_relative '../lib/user_input.rb'
 
 describe UserInput do
 
+  let(:user_input) { UserInput.new }
+
+  describe "#read_line" do
+
+    it "returns a string corresponding to user input from $stdin" do
+      
+        mock_input = "I, the user, typed this in"
+
+        allow($stdin).to receive(:gets).and_return(mock_input)
+        
+        expect(user_input.read_line).to eq(mock_input)
+
+    end
+
+  end
+
 end
