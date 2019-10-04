91a44c90433697e2b1ec45f124b97162ee75eb26
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-04 12:52:26 -0400

    Minimax test: jot down ideas for more test scenarios in test file

--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -8,7 +8,7 @@ describe Minimax do
 
      describe "when computer player goes first with O marker" do 
 
-       it "returns the last box number as a string when the O marker can win by filling in the last box and all other boxes have been taken" do 
+       it "returns the last box number as a string to win when all other boxes have been taken" do 
           board = ThreeByThreeTTTBoard.new(["O", "X", "O", 
                                             "X", "O", "X", 
                                             "X", "O", " "])
@@ -16,8 +16,63 @@ describe Minimax do
           expect(Minimax.find_best_move(board)).to eq("9")
        end
 
+       it "returns the first box number as a string to win when all other boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new([" ", "X", "O", 
+                                            "X", "O", "X", 
+                                            "X", "O", "O"])
+
+          expect(Minimax.find_best_move(board)).to eq("1")
+       end
+
      end
 
-  end
+       it "returns a box number of the board that is not the first or last (as a string) to win when all other boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new(["O", "X", "O", 
+                                            " ", "X", "X", 
+                                            "O", "O", "X"])
 
+          expect(Minimax.find_best_move(board)).to eq("4")
+       end
+
+  end
+       
+       it "returns a box number of the board that is not the first or last (as a string) to win when six boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new(["O", "X", "O", 
+                                            "X", "O", "X", 
+                                            " ", " ", " "])
+
+          expect(Minimax.find_best_move(board)).to eq("7")
+       end
+       
+       it "returns a box number of the board that is not the first or last (as a string) to win when four boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new(["X", "O", "X", 
+                                            " ", "O", "X", 
+                                            " ", " ", " "])
+
+          expect(Minimax.find_best_move(board)).to eq("8")
+       end
+
+       it "returns a box number of the board (as a string) to block when O marker cannot win and four boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new(["X", "O", "O", 
+                                            " ", " ", " ", 
+                                            "X", " ", " "])
+
+          expect(Minimax.find_best_move(board)).to eq("4")
+       end
+
+       it "returns a box number of the board (as a string) to block when O marker cannot win and six boxes have been taken" do 
+          board = ThreeByThreeTTTBoard.new(["O", "O", "X", 
+                                            " ", " ", "O", 
+                                            " ", "X", "X"])
+
+          expect(Minimax.find_best_move(board)).to eq("7")
+       end
+
+       it "returns a box number of the board that is (as a string) to win when the O marker can either block or win" do
+          board = ThreeByThreeTTTBoard.new(["X", "O", "X", 
+                                            " ", "O", "X", 
+                                            " ", " ", " "])
+
+          expect(Minimax.find_best_move(board)).to eq("8")
+       end
 end
