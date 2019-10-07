57176fa72ab4611ba7044bdf6aa4b9f82695152d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-07 13:22:01 -0400

    Computer Player tests: pass refactored test for set_ai and make_selection

--- lib/three_by_three_ttt_computer_player.rb
+++ lib/three_by_three_ttt_computer_player.rb
@@ -2,19 +2,14 @@ require_relative './player.rb'
 
 class ThreeByThreeTTTComputerPlayer < Player
   
-  def make_selection(board)
-    @ai.find_best_move(board)
-  end
+  attr_reader :ai
 
   def set_ai(ai)
     @ai = ai
   end
-#    if board.contents_at_box("5") == " "
-#      "5"
-#    else
-#      index_selected = board.data.find_index { |box| box == " " }
-#      (index_selected + 1).to_s
-#     end
-#  end
+
+  def make_selection(board)
+    @ai.find_best_move(board)
+  end
 
 end
--- spec/three_by_three_ttt_computer_player_spec.rb
+++ spec/three_by_three_ttt_computer_player_spec.rb
@@ -4,36 +4,30 @@ require_relative '../lib/three_by_three_ttt_board.rb'
 
 describe ThreeByThreeTTTComputerPlayer do
 
-  let(:ai_double) { double("ai double") }
-  let(:board) { double("board") }
-  let(:computer_player) { ThreeByThreeTTTComputerPlayer.new(ai: ai_double) }
+  let(:computer_player) { ThreeByThreeTTTComputerPlayer.new }
 
   it "is a subclass of the Player class" do
     expect(ThreeByThreeTTTComputerPlayer.superclass).to eq(Player)
   end
 
+  describe "#set_ai" do
+    it "sets the instance variable @ai" do 
+      ai_double = double("ai double")
+      computer_player.set_ai(ai_double)
+
+      expect(computer_player.ai).to eq(ai_double)
+    end
+  end
+
   describe "#make_selection" do 
     it "calls ai.find_best_move and passes it the board" do
-      expect(:ai_double).to receive(:find_best_move)
+      ai = double("ai double")
+      board = double("board")
+      computer_player.set_ai(ai)
+      
+      expect(ai).to receive(:find_best_move).with(board)
       computer_player.make_selection(board)
     end
   end
 
-  xdescribe "#make_selection(board)" do
-
-    it "should select the middle box if it is open" do 
-      board = ThreeByThreeTTTBoard.new
-      expect(computer_player.make_selection(board)).to eq("5")
-    end
-
-    it "selects the first empty box if the middle box is taken" do       
-      board = ThreeByThreeTTTBoard.new([" ", " ", " ", " ", "X", " ", " ", " ", " "])
-      expect(computer_player.make_selection(board)).to eq("1")
-    end
-
-    it "selects the first empty box if the middle box and other boxes are taken" do
-      board = ThreeByThreeTTTBoard.new(["X", "O", " ", " ", "X", " ", " ", " ", " "])
-      expect(computer_player.make_selection(board)).to eq("3")
-    end
-  end
  end
