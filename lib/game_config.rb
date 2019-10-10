00e073eaeeb243f76124b8e8759a5048ad81ebb8
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-10 15:01:53 -0400

    Spike out GameConfig to lay out anticipated dependencies that need to be defined

--- lib/game_config.rb
+++ lib/game_config.rb
@@ -5,18 +5,26 @@ class GameConfig
 
   def set_up
     { 
-      board: ThreeByThreeTTTBoard.new,
-      rules: ThreeByThreeTTTRules.new,
+      board: board_config,
+      rules: rules_config,
       user_interface: user_interface_config, 
 #      players: player_config.set_up
       players: [] 
     }  
   end
 
+  def rules_config
+    ThreeByThreeTTTRules.new
+  end
+
+  def board_config
+    ThreeByThreeTTTBoard.new
+  end
+
   def player_config
     PlayerConfig.new(
-      output_stream: ConsoleOutputStream.new,
-      input_stream: ConsoleInputStream.new
+      user_interface: user_interface_config,
+      player_factory: player_factory_config
       )
   end
 
@@ -39,4 +47,20 @@ class GameConfig
       messages: messages_config
       )
   end
+
+  def player_factory_config
+    PlayerFactory.new(
+      human_model: HumanPlayer
+      computer_model: ComputerPlayer
+      ai: ai_config 
+      )
+  end
+
+  def ai_config
+    Minimax.new(
+      rules: rules_config
+      )
+  end
+  
+
 end
