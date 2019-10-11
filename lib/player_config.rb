9fabae89f324216591e132ea58b4b79c6293dd1f
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-11 12:51:27 -0400

    PlayerConfig: write first test for #set_up and refactor code. Test still failing.

--- lib/player_config.rb
+++ lib/player_config.rb
@@ -11,16 +11,9 @@ class PlayerConfig
   end
 
   def set_up
-    render_welcome_message_to_user
-    cycle_through_players
-  end
-
-  def render_welcome_message_to_user 
-    output_stream.render(welcome_message)
   end
 
   def cycle_through_players
-    output_stream.render(is_player_human_message?("1"))
   end
    
 end
--- spec/player_config_spec.rb
+++ spec/player_config_spec.rb
@@ -1,19 +1,21 @@
 require_relative '../lib/player_config.rb'
+require_relative '../lib/human_player.rb'
+require_relative '../lib/three_by_three_ttt_computer_player.rb'
 
 describe PlayerConfig do
 
   let(:user_interface_double) { double("user interface") }
   let(:player_factory_double) { double("player factory") }
-  let(:human_player_model_double) { double("human model") }
-  let(:computer_player_model_double) { double("computer model") }
+  let(:human_player_model) { HumanPlayer }
+  let(:computer_player_model) { ThreeByThreeTTTComputerPlayer }
   let(:ai_double) { double("ai") }
 
   let(:player_config) { 
     PlayerConfig.new(
       user_interface: user_interface_double,
       player_factory: player_factory_double,
-      human_player_model: human_player_model_double,
-      computer_player_mode: computer_player_model_double,
+      human_player_model: human_player_model,
+      computer_player_model: computer_player_model,
       ai: ai_double)
      }
   
@@ -23,13 +25,25 @@ describe PlayerConfig do
       PlayerConfig.new(
         user_interface: user_interface_double,
         player_factory: player_factory_double,
-        human_player_model: human_player_model_double,
-        computer_player_model: computer_player_model_double,
+        human_player_model: human_player_model,
+        computer_player_model: computer_player_model,
         ai: ai_double)
        }.to_not raise_error
 
     expect{ 
       PlayerConfig.new
        }.to raise_error.with_message(/missing keywords: user_interface, player_factory, human_player_model, computer_player_model, ai/)
+  
   end
+
+  describe "#set_up" do
+
+    it "returns an array of two human players when user_interface.get_player_type returns 1 twice" do
+      allow(user_interface_double).to receive(:get_player_type).and_return("1", "1")
+      expect(player_config.set_up[0]).to be_a(HumanPlayer)
+      expect(player_config.set_up[1]).to be_a(HumanPlayer)
+    end
+  
+  end
+
 end 
