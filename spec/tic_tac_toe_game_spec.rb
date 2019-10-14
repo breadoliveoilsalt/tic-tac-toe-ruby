fe04a1784a9913848b376f26dde767728f6cbd20
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-14 11:48:26 -0400

    Game Test: draft first test for game loop. To see if pass possible

--- spec/tic_tac_toe_game_spec.rb
+++ spec/tic_tac_toe_game_spec.rb
@@ -3,20 +3,29 @@ require_all 'lib'
 
 describe TicTacToeGame do
 
- let(:board) { instance_double("Board") }
- let(:rules) { instance_double("Rules") }
- let(:user_interface) { instance_double("ConsoleUserInterface") }
- let(:human_player) { instance_double("HumanPlayer") }
- let(:computer_player) { instance_double("ComputerPlayer") }
+ let(:board_double) { instance_double("Board") }
+ let(:rules_double) { instance_double("Rules") }
+ let(:user_interface_double) { instance_double("ConsoleUserInterface") }
+ let(:human_player_double) { instance_double("HumanPlayer") }
+ let(:computer_player_double) { instance_double("ComputerPlayer") }
 
  let(:game) { TicTacToeGame.new(
-   {  board: board,
-      rules: rules,
-      user_interface: user_interface,
-      players: [human_player, computer_player]
+   {  board: board_double,
+      rules: rules_double,
+      user_interface: user_interface_double,
+      players: [human_player_double, computer_player_double]
    })}
 
   it "requires initialization with a hash" do 
     expect{ TicTacToeGame.new }.to raise_error.with_message("wrong number of arguments (given 0, expected 1)")
+  end
+
+  describe "#play_game" do
+
+    it "calls #implement_turn for so long as rules.game_over?(board) returns false" do
+      allow(rules_double).to receive(:game_over?).with(board_double).and_return(false, false, false, true)
+
+      expect(game).to receive(:implement_turn).exactly(3).times
+    end
   end
 end
