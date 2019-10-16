8f54a605f125b5521865d86ad317d3b46e80cb44
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-16 11:59:07 -0400

    DRY up allow/receive calls in player_config_spec

--- spec/player_config_spec.rb
+++ spec/player_config_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/player_config.rb'
 require_relative '../lib/human_player.rb'
 require_relative '../lib/computer_player.rb'
+require_relative '../lib/console_user_interface.rb'
+require_relative '../lib/minimax.rb'
 
 describe PlayerConfig do
 
-  let(:user_interface_double) { double("user interface") }
+  let(:user_interface_double) { instance_double("ConsoleUserInterface") }
   let(:human_player_model) { HumanPlayer }
   let(:computer_player_model) { ComputerPlayer }
-  let(:ai_double) { double("ai") }
+  let(:ai_double) { instance_double("Minimax") }
 
   let(:player_config) { 
     PlayerConfig.new(
@@ -35,37 +37,33 @@ describe PlayerConfig do
 
   describe "#set_up" do
 
-    it "returns an array of two human players when user_interface.get_player_type returns 1 twice" do
+    before(:each) do
       allow(user_interface_double).to receive(:show_welcome)
-      allow(user_interface_double).to receive(:get_player_type).and_return("1", "1")
       allow(user_interface_double).to receive(:get_name)
       allow(user_interface_double).to receive(:get_marker)
+    end
+
+    it "returns an array of two human players when user_interface.get_player_type returns 1 twice" do
+      allow(user_interface_double).to receive(:get_player_type).and_return("1", "1")
       expect(player_config.set_up[0]).to be_a(HumanPlayer)
       expect(player_config.set_up[1]).to be_a(HumanPlayer)
     end
   
     it "returns an array of two computer players when user_interface.get_player_type returns 2 twice" do
-      allow(user_interface_double).to receive(:show_welcome)
       allow(user_interface_double).to receive(:get_player_type).and_return("2", "2")
-      allow(user_interface_double).to receive(:get_name)
-      allow(user_interface_double).to receive(:get_marker)
       expect(player_config.set_up[0]).to be_a(ComputerPlayer)
       expect(player_config.set_up[1]).to be_a(ComputerPlayer)
     end
 
     it "it calls user_interface.get_name and sets the return value to the players' names" do
-      allow(user_interface_double).to receive(:show_welcome)
       allow(user_interface_double).to receive(:get_player_type).and_return("1", "2")
       allow(user_interface_double).to receive(:get_name).and_return("Billy", "Hank")
-      allow(user_interface_double).to receive(:get_marker)
       expect(player_config.set_up[0].name).to eq("Billy")
       expect(player_config.set_up[1].name).to eq("Hank")
     end
       
     it "it calls user_interface.get_marker and sets the return value to the players' markers" do
-      allow(user_interface_double).to receive(:show_welcome)
       allow(user_interface_double).to receive(:get_player_type).and_return("1", "2")
-      allow(user_interface_double).to receive(:get_name)
       allow(user_interface_double).to receive(:get_marker).and_return("X", "O")
       expect(player_config.set_up[0].marker).to eq("X")
       expect(player_config.set_up[1].marker).to eq("O")
