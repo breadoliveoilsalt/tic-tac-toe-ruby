27eb3568bf43201389b4024dba6450cd621468b2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-16 14:26:43 -0400

    Validator: add test and code for marker selection

---
+++ lib/player_factory_validations.rb
@@ -0,0 +1,7 @@
+class PlayerFactoryValidations
+
+  def marker_selection_valid?(player_selection)
+    player_selection.length == 1
+  end
+
+end
---
+++ spec/player_factory_validations_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../lib/player_factory_validations.rb'
+
+describe PlayerFactoryValidations do
+
+  let(:validator) { PlayerFactoryValidations.new }
+
+  describe "#marker_selection_valid?" do
+
+    it "returns true when the argument string has length of 1" do
+      player_selection = "X"
+
+      result = validator.marker_selection_valid?(player_selection)
+
+      expect(result).to eq(true)
+    end
+
+    it "returns false when the argument string has length of 0" do
+      player_selection = ""
+
+      result = validator.marker_selection_valid?(player_selection)
+
+      expect(result).to eq(false)
+    end
+
+    it "returns false when the argument string length greater than 1" do
+      player_selection = "Oo"
+
+      result = validator.marker_selection_valid?(player_selection)
+
+      expect(result).to eq(false)
+    end
+  end
+
+
+end
